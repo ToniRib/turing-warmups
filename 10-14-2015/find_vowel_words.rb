5a9d7a7317b070f6df3f1924ea93455c4d3e852a
ToniRib <tonimarierib@gmail.com>
2015-12-02 08:57:04 -0700

    updates answers for 10-14-2015 from 12-2-2015

--- 10-14-2015/find_vowel_words.rb
+++ 10-14-2015/find_vowel_words.rb
@@ -1,9 +1,17 @@
+def vowel?(l)
+  l == 'a' || l == 'e' || l == 'i' || l == 'o' || l == 'u' || l == 'y'
+end
+
 vowel_file = File.open('vowel-file.txt', 'w')
 
 File.open('wordlist.txt', 'r') do |f|
-  f.each_line do |line|
-    if /[^aeiouy]*a{1}[^aeiouy]*e{1}[^aeiouy]*i{1}[^aeiouy]*o{1}[^aeiouy]*u{1}[^aeiouy]*y{1}[^aeiouy]*/.match(line)
-      vowel_file.write(line)
+  f.each_line do |w|
+    letter_index = [w.index('a'), w.index('e'), w.index('i'), w.index('o'), w.index('u'), w.index('y')]
+    if (letter_index.length == letter_index.compact.length) && (letter_index.sort == letter_index)
+      vowels_only = w.chars.delete_if { |l| !vowel?(l) }
+      if vowels_only.uniq.length == vowels_only.length
+        vowel_file.write(w)
+      end
     end
   end
 end

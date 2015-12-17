f1288ece1ac44aacdba355f1af668aada787f57d
ToniRib <tonimarierib@gmail.com>
2015-12-17 08:59:21 -0700

    solution for dec 17

--- 12-17-2015/word_frequency.rb
+++ 12-17-2015/word_frequency.rb
@@ -1,9 +1,25 @@
 class WordFrequency
   def list_frequencies(words)
     freq = Hash.new(0)
-    words.each do |word|
-      freq[word] += 1
+    words.each { |word| freq[word] += 1 }
+    top_5(sort_by_descending_value(freq))
+  end
+
+  def sort_by_descending_value(frequencies)
+    frequencies.sort_by { |word, count| [-count, word] }
+  end
+
+  def top_5(frequencies)
+    frequencies.take(5).to_h
+  end
+end
+
+class FileReader
+  def read(filename)
+    words = []
+    File.readlines(filename, 'r').each do |line|
+      words << line.chomp.split(" ")
     end
-    freq
+    words.flatten
   end
 end
--- 12-17-2015/word_frequency_test.rb
+++ 12-17-2015/word_frequency_test.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative 'word_frequency'
 require 'minitest/autorun'
 require 'minitest/pride'
@@ -15,7 +16,41 @@ class WordFrequencyTest < Minitest::Test
     assert_equal expected, counter.list_frequencies(words)
   end
 
-  def test
+  def test_it_counts_multiples_of_words
+    counter = WordFrequency.new
+    words = %w(hi hello hi kitty kitty cat cat cat meow)
+    expected = { "hi" => 2,
+                 "hello" => 1,
+                 "kitty" => 2,
+                 "cat" => 3,
+                 "meow" => 1 }
 
+    assert_equal expected, counter.list_frequencies(words)
+  end
+
+  def test_it_lists_only_the_top_five_words_in_descending_order
+    counter = WordFrequency.new
+    words = %w(hi hello hello hi kitty kitty cat cat cat meow meow meow purr)
+    expected = { "cat" => 3,
+                 "meow" => 3,
+                 "hello" => 2,
+                 "hi" => 2,
+                 "kitty" => 2 }
+
+    assert_equal expected, counter.list_frequencies(words)
+  end
+
+  def test_it_lists_top_five_words_from_file
+    counter = WordFrequency.new
+    words = FileReader.new.read('i_have_a_dream.txt')
+
+    expected = { "the" => 137,
+                 "of" => 111,
+                 "a" => 90,
+                 "and" => 87,
+                 "to" => 87
+    }
+
+    assert_equal expected, counter.list_frequencies(words)
   end
 end
